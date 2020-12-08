513915fa2c7d0e79bd643aa4057a7ef123dbc3dd
aves <aveselov@gmail.com>
2020-12-08 17:00:14 +0300

    f

--- components/ReactBlueImpGallery/Slide/index.js
+++ components/ReactBlueImpGallery/Slide/index.js
@@ -72,7 +72,6 @@ class Slide extends React.Component {
       type,
       pw,
       ph,
-      tumbstyle,
       template,
       children, // eslint-disable-line no-unused-vars
       synchedData, // eslint-disable-line no-unused-vars
@@ -86,7 +85,7 @@ class Slide extends React.Component {
       React.cloneElement(template, { ...Object.assign({}, props, { 'data-trigger' : true }) })
     : (
         <a href={src} title={_title} type={type} {...Object.assign({}, props, { 'data-trigger': true })}>
-            <img width={pw} height={ph} src={tn} alt={_title} style={tumbstyle} loading='lazy' />
+            <img width={pw} height={ph} src={tn} alt={_title} style={{border: 1 + 'px solid #333', margin: 2+px}} loading='lazy' />
         </a>
       );
   }
@@ -101,7 +100,6 @@ Slide.propTypes = {
   type: PropTypes.string,
   pw: PropTypes.any,
   ph: PropTypes.any,
-  tumbstyle: PropTypes.string,
   template: PropTypes.any,
   synchedData: PropTypes.object,
 };
@@ -115,7 +113,6 @@ Slide.defaultProps = {
   type: 'image/jpeg',
   pw: 70,
   ph: 70,
-  tumbstyle: 'border: 1px solid #000',
   template: null,
   synchedData: {},
 };
